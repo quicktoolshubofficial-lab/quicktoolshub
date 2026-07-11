29734769019a0896c9a82d38e7db4aa43023be06
quicktoolshubofficial-lab <quicktoolshub.official@gmail.com>
2026-07-11 15:31:44 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  console.log("QuickTools Hub Started");
+
+  const button = document.querySelector("button");
+
+  button.addEventListener("click", () => {
+    alert("Welcome to QuickTools Hub 🚀");
+  });
+
+});
