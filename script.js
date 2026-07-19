a2a981c31881bcb0abf835ad9cb132fdc24085e6
quicktoolshubofficial-lab <quicktoolshub.official@gmail.com>
2026-07-20 02:34:36 +0500

    Update script.js

--- script.js
+++ script.js
@@ -210,3 +210,18 @@ overlay.classList.remove("active");
 });
 
 }
+// ===============================
+// Glass Header Effect
+// ===============================
+
+window.addEventListener("scroll", () => {
+
+    const header = document.querySelector("header");
+
+    if (window.scrollY > 30) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+
+});
