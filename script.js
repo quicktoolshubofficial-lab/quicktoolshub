2078b5bee101dfdcdfb37211e696e83e7a6ea2bf
quicktoolshubofficial-lab <quicktoolshub.official@gmail.com>
2026-07-20 02:46:57 +0500

    Update script.js

--- script.js
+++ script.js
@@ -238,3 +238,27 @@ document.querySelectorAll(".primary-btn, .secondary-btn").forEach(button=>{
     });
 
 });
+// ===============================
+// Back To Top Button
+// ===============================
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 300) {
+        backToTop.classList.add("show");
+    } else {
+        backToTop.classList.remove("show");
+    }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
